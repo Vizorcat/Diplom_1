4a7e7029f3c84c474ede149b07fa19467cae6aed
Artem Pavlov <vizorcat@yandex.ru>
2025-04-22 15:43:15 +0300

    Параметризация и моки. Тесты + отчет тестирования.

--- praktikum/burger.py
+++ praktikum/burger.py
@@ -1,7 +1,6 @@
 from typing import List
-
-from praktikum.praktikum import Bun
-from praktikum.praktikum import Ingredient
+from praktikum.bun import Bun
+from praktikum.ingredient import Ingredient
 
 
 class Burger:
@@ -9,7 +8,7 @@ class Burger:
     Модель бургера.
     Бургер состоит из булочек и ингредиентов (начинка или соус).
     Ингредиенты можно перемещать и удалять.
-    Можно распечать чек с информацией о бургере.
+    Можно распечатать чек с информацией о бургере.
     """
 
     def __init__(self):
